36700459f6bb7e8bc77a8e3343e616d85821bbe9
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-02 22:13:14 +0900

    .

---
+++ user.js
@@ -0,0 +1,16 @@
+const userForm=document.querySelector(".js_userForm"),
+userInput=userForm.querySelector("input"),
+Username=document.querySelector(".js_User");
+
+const USER_NA="currentUsername";
+
+function loadName(){
+    const currentUsername=localStorage.getItem(USER_NA);
+}
+
+
+function init(){
+
+}
+
+init();
